02a49daa82f4d640c4e38f2ebae7066bc8cbd857
arfan <arfanihidayat@gmail.com>
2022-09-01 13:43:27 +0800

    update layout

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -2,8 +2,8 @@
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="{{ asset('vendor/admin/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">SIJELAPP <i class="badge badge-danger ml-3">new</i></span>
+      <img src="{{ Storage::url('logo.png') }}" alt="AdminLTE Logo" class="brand-image elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-bold">SIJELAPP <i class="badge badge-danger ml-3">new</i></span>
     </a>
 
     <!-- Sidebar -->
