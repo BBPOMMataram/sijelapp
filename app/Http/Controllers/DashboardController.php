f532b36067d6efcd350b19404a5c9267749d6483
arfan <arfanihidayat@gmail.com>
2022-09-14 10:23:54 +0800

    update data dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\TerimaSampel;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         $title = 'Dashboard';
-        return view('admin.index', compact('title'));
+        $data = [
+            'userCount' => User::all()->count(),
+            'allSampleThisYear' => TerimaSampel::whereYear('created_at', now()->year)->sum('jumlah_sampel'),
+            'sampleToday' => TerimaSampel::whereDay('created_at', now()->day)
+                ->whereMonth('created_at', now()->month)
+                ->whereYear('created_at', now()->year)
+                ->sum('jumlah_sampel'),
+            'sampleNapzaThisYear' => TerimaSampel::where('id_kategori', 1)->whereYear('created_at', now()->year)->sum('jumlah_sampel'), //1 id kategori napza
+        ];
+        return view('admin.index', compact('title', 'data'));
     }
 }
